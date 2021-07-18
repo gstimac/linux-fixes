e866d24d00888fe353f35ea82aa4a514743f938e
Goran Stimac <goran@email.cm>
2021-07-18 10:58:29 +0200

    Added kubectl and helm

--- ubuntu-20.04-post-install.sh
+++ ubuntu-20.04-post-install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-sudo apt install apt-transport-https curl vim git tlp vlc qbittorrent &&
+sudo apt -qq install -y apt-transport-https curl vim git tlp vlc qbittorrent
 
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
 tfenv install 1.0.2 && tfenv use 1.0.2
 
-wget "https://download.jetbrains.com/python/pycharm-community-2021.1.3.tar.gz" &&
-tar xvfz pycharm-community-2021.1.3.tar.gz &&
+wget -q "https://download.jetbrains.com/python/pycharm-community-2021.1.3.tar.gz" &&
+tar xfz pycharm-community-2021.1.3.tar.gz &&
 mv pycharm-community-2021.1.3 ~
+
+curl -s https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt -qq update
+sudo apt -qq install -y helm
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt -qq update 
+sudo apt -qq install -y kubectl
 
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
